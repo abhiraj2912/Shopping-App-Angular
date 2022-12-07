8713c50432ddac1a98a573455dcf5d73a8da340d
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-08 00:34:05 +0530

    services

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-user-register',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent {
+
+  constructor(private api:ApiService){}
+
+  name=""
+  address=""
+  phone=""
+  email=""
+  password=""
+  cpassword=""
+
+  readValues=()=>{
+    if (this.password==this.cpassword) {
+      let data:any={"name":this.name,"address":this.address,"phone":this.phone,"email":this.email,"password":this.password}
+      this.api.addUser(data).subscribe(
+        (response:any)=>{
+          console.log(response)
+          if (response.status=="success") {
+            alert("User Registered Successfully")
+          } else {
+            alert("Something went wrong")
+          }
+        }
+      )
+      
+    } else {
+      alert("Password Not Matching")
+      
+    }
+  }
 
 }
